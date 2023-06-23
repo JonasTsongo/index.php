b2c1b83057be4ad6730734bcdd741137d96922c9
JonasTsongo <jonastsongo2@gmail.com>
2023-06-23 17:57:01 +0200

    ajout nouveau projet

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author"  content="JONAS">
+    <meta name="description"  content="lorem">
+
+    <link rel="icon" type="image/jpg" size="32x32" href="images/j.jpg"/>
+    <link rel="apple-touch-icon" type="image/jpg" size="32x32" href="images/j.jpg"/>
+    <link rel="stylesheet" href="assets/style.css"/>
+    <title>Exercice</title>
+</head>
+<body>
+
+    <nav class="navigation" id="navigation">
+        <div>
+             <img class="logo" src="images/j.jpg"/>
+        </div>
+        <ul>
+            <li><a href="Acceuil.php">Acceuil</a></li>
+            <li><a href="apropos.php">Apropos</a></li>
+            <li><a href="Blog.php">Blog</a></li>
+        </ul>
+    </nav>
+    
+</body>
+<footer>
+
+</footer>
+</html>
