7029279e35261265d3448a9973136c0dd21e1ad1
JonasTsongo <jonastsongo2@gmail.com>
2023-07-03 16:02:22 +0200

    premier experience responsive

--- index.php
+++ index.php
@@ -3,39 +3,87 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author"  content="JONAS">
     <meta name="description"  content="lorem">
     <link rel="icon" type="image/jpg" size="32x32" href="images/j.jpg"/>
     <link rel="apple-touch-icon" type="image/jpg" size="32x32" href="images/j.jpg"/>
     <link rel="stylesheet"href="assets/style.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <title>Exercice</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-<nav class="navigation" id="navigation">
-        <div>
-             <img class="logo" src="images/j.jpg"/>
-        </div>
+<nav class="presentation" id="presentation">
         <ul>
             <li><a href="Acceuil.php">Acceuil</a></li>
             <li><a href="apropos.php">Apropos</a></li>
-            <li><a href="Blog.php">Blog</a></li>
+            <li><a href="Blog.php">Contact</a></li>
         </ul>
-    </nav>
-    
-<img class="img-cjelb"src="images/cjelb.jpg" alt="coordinnation de la jeunesse estudiantine de lubero">
-  <div class="wrapper"> 
+</nav>
+<img class="img-cjelb"src="images/Y2.jpg" alt="Yetu">
+    <div class="wrapper"> 
     <article class="img-info"> 
-        <h2>Yay</h2>
+        <br>
+        <h2>BIENVENU SUR LE SITE DES ETS YETU RDC</h2>
+
+          <br>
         <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, explicabo commodi.
              Voluptate explicabo eaque, cumque ipsam dicta porro dolorem expedita. Officiis, modi.
               Ipsum quaerat vitae praesentium omnis, ducimus eaque. Facilis cum, voluptatibus eum eaque 
               earum nesciunt, ex adipisci delectus repellat quod sunt nam, dignissimos
              molestiae? Officia optio fuga ipsam quam dignissimos aperiam veritatis tempore tenetur accusantium!</p>
+
+            <br>
+            <P> 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe dolorem dignissimos libero nesciunt 
+             assumenda odit consequuntur dolorum sapiente distinctio illo, magni ex dolor esse quidem.
+              Quos quae tenetur ex explicabo recusandae atque similique quo veniam porro ab. Voluptatibus, quod
+               obcaecati!
+            </P>   
     </article>
-    <img class="img-j"src="images/j.jpg" alt="john">
-  </div>
+    <img class="img-Y"src="images/Y.jpg" alt="john">
+    <footer class="footer"> 
+         <div class="container"> 
+             <div class="row"> 
+                 <div class="footer-col"> 
+                   <h4>Company</h4>
+                   <ul> 
+                       <li><a href="#">About Us</a></li>
+                       <li><a href="#">Our Services</a></li>
+                       <li><a href="#">Privancy Polices</a></li>
+                       <li><a href="#">Affiliate Program</a></li>
+                   </ul>
+                 </div>
+                 <div class="footer-col"> 
+                   <h4>Get Help</h4>
+                   <ul> 
+                       <li><a href="#">FAQ</a></li>
+                       <li><a href="#">shopping</a></li>
+                       <li><a href="#">returs</a></li>
+                       <li><a href="#">orders status</a></li>
+                       <li><a href="#">option payments</a></li>
+                   </ul>
+                 </div>
+                 <div class="footer-col"> 
+                   <h4>Online Shop</h4>
+                   <ul> 
+                       <li><a href="#">Wacth</a></li>
+                       <li><a href="#">Bag</a></li>
+                       <li><a href="#">Shoes</a></li>
+                       <li><a href="#">Adress</a></li>
+                   </ul>
+                 </div>
+                 <div class="footer-col"> 
+                   <h4>Follow Us</h4>
+                   <div class="social-links"> 
+                     <a href=""><i class="fab fa-facebook-f"></i></a>
+                     <a href=""><i class="fab fa-twitter"></i></a>
+                     <a href=""><i class="fab fa-instagram"></i></a>
+                     <a href=""><i class="fab fa-linked-in"></i></a>
+                   </div> 
+                 </div>
+             </div>
+         </div>
+</footer>   
 </body>
-<footer>
-</footer>
 </html>
